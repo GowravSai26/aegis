602422fd6348d2e9dec5bc422ab727a0adf175fa
GowravSai26 <gowravnagsai@gmail@example.com>
2026-04-19 11:19:24 +0000

    fix: update CORS for Vercel frontend

--- api/main.py
+++ api/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="AEGIS", description="Autonomous Chargeback Defense System",
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://aegis-zeta-ten.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
